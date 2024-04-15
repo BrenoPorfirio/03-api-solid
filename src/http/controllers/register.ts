909c100b0a4412e99bff5cdd74445f0c4f644f9a
Breno Porfirio <brenoporfirio03@gmail.com>
2024-04-15 20:16:55 -0300

    repository interface

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,4 @@
-import { PrismaUserRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
