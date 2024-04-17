9a4464e5579944370bc74f2819304d9b532f6055
Breno Porfirio <brenoporfirio03@gmail.com>
2024-04-17 19:00:45 -0300

    test to register

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -5,6 +5,19 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { UserAlreadyExistsError } from './errors/user-already-exists.error'
 
 describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUserCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUserCase.execute({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it('should hash user password upon registration', async () => {
     const usersRepository = new InMemoryUsersRepository()
     const registerUserCase = new RegisterUseCase(usersRepository)
