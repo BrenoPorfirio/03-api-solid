669fa1207bfec5206cad5cdb86b9abb82f497086
Breno Porfirio <brenoporfirio03@gmail.com>
2024-05-01 18:40:35 -0300

    e2e test authenticate

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,27 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Authenticate (e2)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('shoud be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+    const response = await request(app.server).post('/sessions').send({
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
